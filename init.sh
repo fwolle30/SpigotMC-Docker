d0b844f5faa8fabebc8896c64d434ece2697d9ac
fwolbring <fwolbring@googlemail.com>
2017-04-22 22:07:50 +0200

    * optimized Build Process

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /e
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 apt-get update
-apt-get -y install curl git debconf-utils
+apt-get -y install git debconf-utils
 
 echo "oracle-java8-installer  oracle-java8-installer/not_exist        error" >> settings.conf
 echo "oracle-java8-installer  shared/present-oracle-license-v1-1      note" >> settings.conf
@@ -15,6 +15,5 @@ debconf-set-selections < settings.conf
 
 apt-get -y install oracle-java8-installer
 
-curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O
 git config --global --unset core.autocrlf
 java -jar BuildTools.jar
